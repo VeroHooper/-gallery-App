bb6f7f1a16bc142b71d0dc8fc7c09bc42ad7c0cf
VeroHooper <vee.hoop21@gmail.com>
2021-09-17 09:12:19 -0700

    Final project

---
+++ lib/userData.dart
@@ -0,0 +1,23 @@
+List userData = [
+  {
+    "userName": "Kwame Jantua",
+    "userLocation": "Adisadel Estate",
+    "userProfileImage": "images/profile_2.jpeg",
+    "userPost": "images/picture_1.jfif",
+    "userPostLikes": "23",
+  },
+  {
+    "userName": "Ama Ansah",
+    "userLocation": "Pedu,Cape Coast",
+    "userProfileImage": "images/profile_2.jpeg",
+    "userPost": "images/picture_2.jfif",
+    "userPostLikes": "23",
+  },
+  {
+    "userName": "Esi Hooper",
+    "userLocation": "Abura,Cape Coast",
+    "userProfileImage": "images/profile_3.jpg",
+    "userPost": "images/picture_3.jfif",
+    "userPostLikes": "23",
+  }
+];
